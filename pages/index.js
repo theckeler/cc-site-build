d6baeb0e9382120dc69f88697d7bfd66911c546b
Todd Heckeler <todd@consumedesign.com>
2023-04-17 14:46:48 -0400

    update

--- pages/index.js
+++ pages/index.js
@@ -14,61 +14,65 @@ import Price from "../components/PDP/Price";
 
 export default function Index() {
 	return (
-		<main className="p-5">
-			<ul className="max-w-screen-2xl md:flex flex-wrap mx-auto">
-				<li className="flex flex-wrap basis-full">
-					<BreadCrumbs />
-				</li>
-				<li className="md:w-8/12 py-4">
-					<div className="md:hidden">
-						<H1 />
-						<H2 />
-						<Price />
-					</div>
-					<div>
-						<PDPImages />
+		<>
+			<div className="bg-gray-400" style={{ minHeight: "160px" }}></div>
+			<main className="p-5">
+				<ul className="max-w-screen-2xl md:flex flex-wrap mx-auto">
+					<li className="flex flex-wrap basis-full">
+						<BreadCrumbs />
+					</li>
+					<li className="md:w-8/12 py-4">
 						<div className="md:hidden">
-							<AddCart />
-						</div>
-						<Reviews />
-						<Features />
-						<Specs />
-						<Maintenance />
-						<DocumentsGuides />
-						<FindParts />
-						<RelatedProducts />
-					</div>
-				</li>
-				<li className="hidden md:block md:w-4/12 sticky top-0 h-full">
-					<div className="md:px-6 py-4">
-						<div className="">
 							<H1 />
 							<H2 />
 							<Price />
 						</div>
-						<AddCart />
-						<ul className="list-disc px-8 text-xs leading-6">
-							<li className="mt-5">
-								A 24HP/725cc Kohler 7000 Series V-Twin OHV Engine, OHV engine
-								delivers high-performance power and exceptionally smooth, quiet
-								operation
-							</li>
+						<div>
+							<PDPImages />
+							<div className="md:hidden">
+								<AddCart />
+							</div>
+							<Reviews />
+							<Features />
+							<Specs />
+							<Maintenance />
+							<DocumentsGuides />
+							<FindParts />
+							<RelatedProducts />
+						</div>
+					</li>
+					<li className="hidden md:block md:w-4/12 sticky top-0 h-full">
+						<div className="md:px-6 py-4">
+							<div className="">
+								<H1 />
+								<H2 />
+								<Price />
+							</div>
+							<AddCart />
+							<ul className="list-disc px-8 text-xs leading-6">
+								<li className="mt-5">
+									A 24HP/725cc Kohler 7000 Series V-Twin OHV Engine, OHV engine
+									delivers high-performance power and exceptionally smooth,
+									quiet operation
+								</li>
 
-							<li className="mt-1">
-								Dual hydrostatic transmissions allow for quick and effortless 7
-								MPH forward (3.5 MPH reverse) ground speeds that leaves your
-								lawn with a professional-quality finish
-							</li>
+								<li className="mt-1">
+									Dual hydrostatic transmissions allow for quick and effortless
+									7 MPH forward (3.5 MPH reverse) ground speeds that leaves your
+									lawn with a professional-quality finish
+								</li>
 
-							<li className="mt-1">
-								54-in. AeroForce fabricated deck that delivers a signature cut
-								with fewer clumps and stragglers, finer clippings and increased
-								evenness
-							</li>
-						</ul>
-					</div>
-				</li>
-			</ul>
-		</main>
+								<li className="mt-1">
+									54-in. AeroForce fabricated deck that delivers a signature cut
+									with fewer clumps and stragglers, finer clippings and
+									increased evenness
+								</li>
+							</ul>
+						</div>
+					</li>
+				</ul>
+			</main>
+			<div className="bg-gray-400" style={{ minHeight: "600px" }}></div>
+		</>
 	);
 }
