2af4cfc0dba47a1b3ef35c424871695bd4d782c8
Todd Heckeler <todd@consumedesign.com>
2023-04-27 08:46:33 -0400

    adding to pdp

--- pages/lawn-mowers/riding-lawn-mowers/zero-turn-mowers/pdp/[slug].js
+++ pages/lawn-mowers/riding-lawn-mowers/zero-turn-mowers/pdp/[slug].js
@@ -12,57 +12,74 @@ import H1 from "@/components/H1";
 import H2 from "@/components/H2";
 import Price from "@/components/Price";
 import Desc from "@/components/Desc";
+import SEOGreek from "@/components/SEOGreek";
+import SEOLinks from "@/components/SEOLinks";
+import ProductBlock from "@/product/Block";
 
 import productsJSON from "@/data/pdp.json";
 
 const IndexID = (id) => {
 	const product = productsJSON.products[Number(id.slug - 1)];
 
-	console.log(product);
-
 	return (
-		<ul className="max-w-screen-2xl grid grid-cols-12 gap-3 mx-auto p-3">
-			<li className="col-span-12">
-				<BreadCrumbs
-					crumbs={[
-						{ title: "Home", url: "/" },
-						{ title: "Lawn Mowers", url: "/lawn-mowers" },
-						{
-							title: "Riding Lawn Mowers",
-							url: "/lawn-mowers/riding-lawn-mowers",
-						},
-						{
-							title: "Zero-Turn Mowers",
-							url: "/lawn-mowers/riding-lawn-mowers/zero-turn-mowers",
-						},
-						{ title: product.title },
-					]}
-					stars={true}
-					starNum={product.reviews}
-				/>
-			</li>
-			<li className="col-span-12 lg:col-span-7 xl:col-span-8">
-				<Gallery gallery={product.gallery} />
-			</li>
-			<li className="col-span-12 lg:col-span-5 xl:col-span-4 lg:sticky lg:top-0 self-start row-span-2">
-				<div className="md:px-6 py-4">
-					<H1 title={product.title} srOnly="Cub Cadet Zero-Turn Mower" />
-					<H2 title={product.series} />
-					<Price price={product.price} msrp={product.msrp} />
-					<AddCart />
-					<Desc blocks={product.desc} />
+		<>
+			<ul className="max-w-screen-2xl grid grid-cols-12 gap-3 mx-auto p-3">
+				<li className="col-span-12">
+					<BreadCrumbs
+						crumbs={[
+							{ title: "Home", url: "/" },
+							{ title: "Lawn Mowers", url: "/lawn-mowers" },
+							{
+								title: "Riding Lawn Mowers",
+								url: "/lawn-mowers/riding-lawn-mowers",
+							},
+							{
+								title: "Zero-Turn Mowers",
+								url: "/lawn-mowers/riding-lawn-mowers/zero-turn-mowers",
+							},
+							{ title: product.title },
+						]}
+						stars={true}
+						starNum={product.reviews}
+					/>
+				</li>
+				<li className="col-span-12 lg:col-span-7 xl:col-span-8">
+					<Gallery gallery={product.gallery} />
+				</li>
+				<li className="col-span-12 lg:col-span-5 xl:col-span-4 lg:sticky lg:top-0 self-start row-span-2">
+					<div className="md:px-6 py-4">
+						<H1 title={product.title} srOnly="Cub Cadet Zero-Turn Mower" />
+						<H2 title={product.series} />
+						<Price price={product.price} msrp={product.msrp} />
+						<AddCart />
+						<Desc blocks={product.desc} />
+					</div>
+				</li>
+				<li className="col-span-12 lg:col-span-7 xl:col-span-8">
+					<Reviews starNum={product.reviews} />
+					<Features />
+					<Specs />
+					<Maintenance />
+					<DocumentsGuides />
+					<FindParts />
+					<RelatedProducts products={productsJSON.relatedProducts} />
+				</li>
+			</ul>
+			<div className="max-w-screen-2xl mx-auto my-20 p-3">
+				<div>
+					<h2 className="text-3xl mb-8 text-center">Recently Viewed</h2>
+					<ProductBlock products={productsJSON.recentlyViewed} />
 				</div>
-			</li>
-			<li className="col-span-12 lg:col-span-7 xl:col-span-8">
-				<Reviews starNum={product.reviews} />
-				<Features />
-				<Specs />
-				<Maintenance />
-				<DocumentsGuides />
-				<FindParts />
-				<RelatedProducts products={productsJSON.relatedProducts} />
-			</li>
-		</ul>
+
+				<div className="mt-20">
+					<SEOLinks />
+				</div>
+
+				<div className="mt-20">
+					<SEOGreek />
+				</div>
+			</div>
+		</>
 	);
 };
 
