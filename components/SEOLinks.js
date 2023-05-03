9557628fea9ccd8bd58fbbe54f2e2362cf15d406
Todd Heckeler <todd@consumedesign.com>
2023-05-03 08:41:49 -0400

    adjust widths

--- components/SEOLinks.js
+++ components/SEOLinks.js
@@ -1,7 +1,7 @@
 export default function SEOLinks() {
 	return (
-		<ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 lg:gap-4">
-			{[...Array(18)].map((x, i) => (
+		<ul className="grid gap-1 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 lg:gap-4">
+			{[...Array(8)].map((x, i) => (
 				<li key={i}>
 					<a
 						href="#top"
